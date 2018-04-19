73ff1f4e0e76c45a34407919cd3727f2e178a6b2
Sharmina Akhtar <sakht318@gmail.com>
2018-04-19 13:36:10 +0000

    automatically backed up by learn

--- db/migrate/add_column_to_taxi.rb
+++ db/migrate/add_column_to_taxi.rb
@@ -1,6 +1,6 @@
 class AddColumnToTaxi < ActiveRecord::Migration
 	def change
-      add_column :taxis, :passenger_id,  :integer 
-      add_column :taxis, :ride_id,  :integer   
+      add_column :taxis, :passenger_id,  :integer
+      add_column :taxis, :ride_id,  :integer
 	end
 end
