012f5232a9acefe4c5d30d52b223ca2673c8cd72
Sharmina Akhtar <sakht318@gmail.com>
2018-04-19 13:41:32 +0000

    automatically backed up by learn

--- db/migrate/add_column_to_passenger.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToPassenger < ActiveRecord::Migration
-	def change
-      add_column :passengers, :taxi_id,  :integer
-      add_column :passengers, :ride_id,  :integer
-	end
-end
--- db/migrate/add_column_to_ride.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToRide < ActiveRecord::Migration
-	def change
-      add_column :rides, :passenger_id,  :integer
-      add_column :rides, :taxi_id,  :integer
-	end
-end
